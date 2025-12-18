64efdc4beac0ec60bffb4536d3864e44370a1e14
Danyell-Backend <daniel.diaz@sorsu.edu.ph>
2025-12-19 01:32:21 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Inventory Management System API',
+        'version' => '1.0.0',
+        'status' => 'running'
+    ]);
+});
